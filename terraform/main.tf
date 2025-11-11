e92cbab5a8a3a062a4ce699abcdf962186de54b8
parthapratim131 <parthapratim131@outlook.com>
2025-11-11 09:01:52 +0530

    Test terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,5 @@
 terraform {
   required_version = ">= 1.0"
-
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
-  }
 }
 
 resource "local_file" "config" {
